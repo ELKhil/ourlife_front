feded9fd84a6e29baad3a4d32f64538dec619217
Mohamed Saad El Khil <m.s.elkhil@gmail.com>
2023-08-08 22:45:54 +0200

    login

--- src/app/Services/login.service.ts
+++ src/app/Services/login.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Auth } from '../Models/auth.model';
 import { Login } from '../Models/login';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +15,6 @@ export class LoginService {
   ) { }
 
   public login(form: Login) : Observable<Auth>{
-    return this._http.post<Auth>('http://localhost:8000/api/login_check', form);
+    return this._http.post<Auth>(environment.api_url + '/api/login_check', form);
   }
 }
