7fe96b959a5269f7f51333db7530f7707ad8188e
Mohamed Saad El Khil <m.s.elkhil@gmail.com>
2023-08-08 11:29:03 +0200

    deploy v1.2

--- serveur.js
+++ serveur.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/ourlife-frontend'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/my-new-project/index.html'));
+res.sendFile(path.join(__dirname+'/dist/ourlife-frontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
