a1487c23240d75c810bfda1bb089bbf9c2436f44
Mohamed Saad El Khil <m.s.elkhil@gmail.com>
2023-08-08 10:38:22 +0200

    deploy v7

--- serveur.js
+++ serveur.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/my-new-project'));
+app.use(express.static(__dirname + '/dist/ourlife-frontend'));
 
 app.get('/*', function(req,res) {
     
