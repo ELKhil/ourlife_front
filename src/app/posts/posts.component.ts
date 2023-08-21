f62b88927b1ade2a4d8e70ad973f992440250805
Mohamed Saad El Khil <m.s.elkhil@gmail.com>
2023-08-21 21:40:47 +0200

    delet comment

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -15,7 +15,7 @@ import { ConfirmDialogComponent, ConfirmDialogModel } from '../confirm-dialog/co
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { environment } from 'src/environments/environment.prod';
 
-declare var toastr :any;
+declare var toastr: any;
 
 
 @Component({
@@ -25,100 +25,94 @@ declare var toastr :any;
 })
 export class PostsComponent implements OnInit {
 
-  posts! : Iposts[];
-  nextPosts! : Iposts[];
+  posts!: Iposts[];
+  nextPosts!: Iposts[];
   notEmptyPost = true;
   notScrolly = true;
   nbPage = 1;
-  fileType : string = "";
+  fileType: string = "";
   showCommentaire: boolean = false;
-  loadComents : any[] = [];
-  loadPostId : string = "0";
-  getPageUserScroll : boolean = false;
-  userName! :string;
-  
+  loadComents: any[] = [];
+  loadPostId: string = "0";
+  getPageUserScroll: boolean = false;
+  userName!: string;
 
-  fg! : UntypedFormGroup;
-  ajoutComment! : string;
+
+  fg!: UntypedFormGroup;
+  ajoutComment: { [key: string]: string } = {};
   result: string = "";
 
   isLogged: boolean = false;
   isLoading: boolean = false;
 
-  
 
-  
+
+
 
   constructor(
-    private fb : UntypedFormBuilder,
+    private fb: UntypedFormBuilder,
     private postsService: PostsService,
     private spinner: NgxSpinnerService,
     private http: HttpClient,
     public session: SessionService,
-    private comentService :ComentService,
-    private router : Router,
+    private comentService: ComentService,
+    private router: Router,
     public dialog: MatDialog,
-    
-   
-   
-   
-  
-  ) {this.loadComents = []; }
+  ) { this.loadComents = []; }
 
 
   ngOnInit(): void {
-
-      //une méthode qui ne charge pas tous les données
-      //this.postsService.getAllPosts().subscribe(data => this.posts = data);
-
-      //Une méthode qui charge page par page
-      this.getAllPublications();
-
-      this.session.isLogged.subscribe(loggedIn => {
+    //Une méthode qui charge page par page
+    this.getAllPublications();
+    this.session.isLogged.subscribe(loggedIn => {
       this.isLogged = loggedIn;
-   
-      });
-      
+
+    });
+
   }
 
-  getAllPublications(){
+  getAllPublications() {
     this.postsService.getPage(this.nbPage).subscribe(data => this.posts = data);
   }
 
   //déclencheur de scroll
-  onScroll(){
-    if(this.getPageUserScroll == false){
-          if(this.notScrolly && this.notEmptyPost){  
-          this.spinner.show();
-          this.notScrolly = false; 
-          this.loadNextPage();
-          
-       }
+  onScroll() {
+    if (this.getPageUserScroll == false) {
+      if (this.notScrolly && this.notEmptyPost) {
+        this.spinner.show();
+        this.notScrolly = false;
+        this.loadNextPage();
+
+      }
     }
-   
+
   }
-  loadNextPage(){
-      this.postsService.getPage(++this.nbPage).subscribe((data: any) => {
-              this.nextPosts = data;
-              if(this.nextPosts.length !== 0){
-                //add the nextPots to the liste posts
-              this.posts = this.posts.concat(this.nextPosts);
-              this.notScrolly= true;
-              this.spinner.hide();
-              
-              }else{
-                this.notEmptyPost = false;
-                this.spinner.hide();
-              }
-              
-      });
-   };
+  loadNextPage() {
+    this.postsService.getPage(++this.nbPage).subscribe((data: any) => {
+      this.nextPosts = data;
+      if (this.nextPosts.length !== 0) {
+        //add the nextPots to the liste posts
+        this.posts = this.posts.concat(this.nextPosts);
+        this.notScrolly = true;
+        this.spinner.hide();
+
+      } else {
+        this.notEmptyPost = false;
+        this.spinner.hide();
+      }
+
+    });
+  };
 
 
-   //montrer les commentaires
-   showComment(postId : string){
+  //montrer les commentaires
+  showComment(postId: string) {
     this.showCommentaire = !this.showCommentaire;
-  
+
+    if (!this.ajoutComment[postId]) {
+      this.ajoutComment[postId] = '';
+    }
+
     // Demandez les commentaires pour le post spécifié
     this.comentService.getMessages(Number(postId)).subscribe(data => {
       // Une fois que vous recevez les données, mettez à jour loadComents et loadPostId
@@ -126,120 +120,122 @@ export class PostsComponent implements OnInit {
       this.loadPostId = postId;
     });
   }
-  
-  
 
 
-   //l'enregistrement d'un  nouveau commentaire
-   submit(postId: string) {
+
+
+  //l'enregistrement d'un  nouveau commentaire
+  submit(postId: string) {
     this.isLoading = true;
 
-    if (this.ajoutComment) {  // Ceci vérifie que ajoutComment n'est pas null, undefined ou une chaîne vide
-        let commentaire: any = {
-            userComImage: "",
-            userComNom: "",
-            contenu: this.ajoutComment,
-            postId: Number(postId),
-            active: false,
+    // Ceci vérifie que ajoutComment[postId] n'est pas null, undefined ou une chaîne vide
+    if (this.ajoutComment[postId]) {  
+      let commentaire: any = {
+        userComImage: "",
+        userComNom: "",
+        contenu: this.ajoutComment[postId],  // mise à jour ici
+        postId: Number(postId),
+        active: false,
+      }
+
+      // Envoie de message
+      this.comentService.post(commentaire).subscribe((response: any) => {
+        toastr.success("Commentaire ajouté ...");
+
+        // Mise à jour de la liste des commentaires affichée à l'utilisateur
+        if (!this.loadComents) {
+          this.loadComents = [];
         }
-
-        // Envoie de message
-        this.comentService.post(commentaire).subscribe((response: any) => {
-            toastr.success("Commentaire ajouté ...");
-
-            // Mise à jour de la liste des commentaires affichée à l'utilisateur
-            if (!this.loadComents) {
-                this.loadComents = [];
-            }
-            this.loadComents.push(response);
-            this.isLoading = false;
-        },
+        this.loadComents.push(response);
+        this.isLoading = false;
+      },
         (error) => {
-            toastr.error(error.error.message);
-            this.isLoading = false;
+          toastr.error(error.error.message);
+          this.isLoading = false;
         });
 
     } else {
-        toastr.error("Message non envoyé..");
-        this.isLoading = false;
+      toastr.error("Message non envoyé..");
+      this.isLoading = false;
     }
 
     this.loadPostId = postId;
-    this.ajoutComment = "";
+    this.ajoutComment[postId] = "";  // mise à jour ici
     this.showCommentaire = true;
 }
 
 
-   postDelet(postId :string){
-        console.log("supprimer ", postId); 
-        this.postsService.delet(Number(postId)).subscribe({
-          next : (p) => {
-            toastr.success("Votre poste a bien été supprimé");
-            location.reload();
-          }, error: () => {
-            toastr.error("Something wrong");
-          }
-      });
-    }
 
-    commentDelet(messageId : string, postId : string){
-      this.comentService.delet(Number(messageId)).subscribe({
-        next : (p) => {
-          toastr.success("Votre commentaire a bien été supprimé");
-          this.comentService.getMessages(Number(postId)).subscribe(data => this.loadComents = data);
-        }, error: () => {
-          toastr.error("Something wrong");
-        }
+  postDelet(postId: string) {
+    console.log("supprimer ", postId);
+    this.postsService.delet(Number(postId)).subscribe({
+      next: (p) => {
+        toastr.success("Votre poste a bien été supprimé");
+        location.reload();
+      }, error: () => {
+        toastr.error("Something wrong");
+      }
     });
-    }
-  
+  }
 
-    postsUser(username :string){
-     
-      this.userName = username;
-      this.getPageUserScroll =true;
-      this.postsService.getPageUser(username).subscribe(data => this.posts = data);
-    }
-
-
-    confirmDialog(postId :string): void {
-      const message = `Voulez-vous vraiment supprimer ce poste?`;
-  
-      const dialogData = new ConfirmDialogModel("Our Life Message", message);
-
-      const dialogRef = this.dialog.open(ConfirmDialogComponent, {
-        maxWidth: "600px",
-        data: dialogData
-      });
-  
-      dialogRef.afterClosed().subscribe(dialogResult => {
-        this.result = dialogResult;
-        console.log(this.result);
-        if(this.result){
-          this.postDelet(postId);
-        }
-      });  
-    }
+  commentDelet(messageId: string, postId: string) {
+    this.comentService.delet(Number(messageId)).subscribe({
+      next: (p) => {
+        toastr.success("Votre commentaire a bien été supprimé");
+        this.comentService.getMessages(Number(postId)).subscribe(data => this.loadComents = data);
+      }, error: () => {
+        toastr.error("Something wrong");
+      }
+    });
+  }
 
 
+  postsUser(username: string) {
 
-    confirmDialogMessage(messageId : string,postId :string ): void {
-      const message = `Voulez-vous vraiment supprimer ce message?`;
-  
-      const dialogData = new ConfirmDialogModel("Our Life Message", message);
+    this.userName = username;
+    this.getPageUserScroll = true;
+    this.postsService.getPageUser(username).subscribe(data => this.posts = data);
+  }
 
-      const dialogRef = this.dialog.open(ConfirmDialogComponent, {
-        maxWidth: "600px",
-        data: dialogData
-      });
-  
-      dialogRef.afterClosed().subscribe(dialogResult => {
-        this.result = dialogResult;
-        console.log(this.result);
-        if(this.result){
-          this.commentDelet(messageId, postId);
-        }
-      });
+
+  confirmDialog(postId: string): void {
+    const message = `Voulez-vous vraiment supprimer ce poste?`;
+
+    const dialogData = new ConfirmDialogModel("Our Life Message", message);
+
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      maxWidth: "600px",
+      data: dialogData
+    });
+
+    dialogRef.afterClosed().subscribe(dialogResult => {
+      this.result = dialogResult;
+      console.log(this.result);
+      if (this.result) {
+        this.postDelet(postId);
+      }
+    });
+  }
+
+
+
+  confirmDialogMessage(messageId: string, postId: string): void {
+    const message = `Voulez-vous vraiment supprimer ce message?`;
+
+    const dialogData = new ConfirmDialogModel("Our Life Message", message);
+
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      maxWidth: "600px",
+      data: dialogData
+    });
+
+    dialogRef.afterClosed().subscribe(dialogResult => {
+      this.result = dialogResult;
+      console.log(this.result);
+      if (this.result) {
+        this.commentDelet(messageId, postId);
+      }
+    });
 
 
   }
