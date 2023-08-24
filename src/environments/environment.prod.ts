84674aec1a5d36d200e233c9c8d309923a242de9
Mohamed Saad El Khil <m.s.elkhil@gmail.com>
2023-08-24 22:45:04 +0200

    fix:bug

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: true,
   api_url: 'https://api-ourlife-fc4625848857.herokuapp.com',
   image_url : "https://res.cloudinary.com/hblnu5cl2/image/upload/v1/",
+  video_url : "https://res.cloudinary.com/hblnu5cl2/video/upload/f_auto:video,q_auto/"
 };
