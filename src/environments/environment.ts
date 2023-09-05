a63dc7beecdbd8c4284173c5227d5890d414131c
Mohamed Saad El Khil <m.s.elkhil@gmail.com>
2023-09-05 21:47:57 +0200

    change image

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  api_url: 'http://localhost:8000',
+  api_url: 'https://api-ourlife-fc4625848857.herokuapp.com',
   image_url : "https://res.cloudinary.com/hblnu5cl2/image/upload/v1/",
   video_url : "https://res.cloudinary.com/hblnu5cl2/video/upload/f_auto:video,q_auto/"
 };
